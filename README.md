2411904b02a34844db6d1be16928dc652d68c1a7
Alex Kayser <akctba@gmail.com>
2020-01-03 10:44:04 -0800

    add socket.io

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # multiroom web chat
+
+- Node.js
+    - body-parser
+    - consign
+    - ejs
+    - express
+    - express-validator
+    - socket.io
--- app.js
+++ app.js
@@ -2,6 +2,17 @@
 var app = require('./config/server');
 
 /* parametrizar a porta de escuta */
-app.listen(80, function() {
+var server = app.listen(80, function() {
     console.log('Server running...');
+});
+
+// adding the websocket protocol to the same port 80
+var io = require('socket.io').listen(server);
+/* criar a conexao por websocket */
+io.on('connection', function(socket){
+    console.log('New user connected');
+
+    socket.on('disconnect', function(){
+        console.log('User disconnected');
+    });
 });
