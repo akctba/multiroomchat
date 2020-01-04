bc9cf43f617e346c1257d7255afa338b30cae22e
Alex Kayser <akctba@gmail.com>
2020-01-04 13:14:31 -0800

    dialogs with websocket

--- app.js
+++ app.js
@@ -8,11 +8,32 @@ var server = app.listen(80, function() {
 
 // adding the websocket protocol to the same port 80
 var io = require('socket.io').listen(server);
+
+app.set('io', io);
+
 /* criar a conexao por websocket */
 io.on('connection', function(socket){
     console.log('New user connected');
 
     socket.on('disconnect', function(){
-        console.log('User disconnected');
+        console.log('User disconnected ');
+        
+
+    });
+
+    socket.on('msgParaServidor', function(data){
+        //DIALOG
+        //to the current user
+        socket.emit(
+            'msgParaCliente', 
+            {apelido: data.apelido, mensagem: data.mensagem}
+        );
+        //to all users except the current
+        socket.broadcast.emit(
+            'msgParaCliente', 
+            {apelido: data.apelido, mensagem: data.mensagem}
+        );
+
+        
     });
 });
--- app/controllers/chat.js
+++ app/controllers/chat.js
@@ -1,14 +1,41 @@
-module.exports.iniciaChat = function(application, req, res) {
+module.exports.iniciaChat = function (application, req, res) {
+
+    let people = application.get('people');
+
     var dadosForm = req.body;
-    req.assert('apelido','Nome ou apelido eh obrigatorio').notEmpty();
-    req.assert('apelido','Nome ou apelido deve conter entre 3 e 15 caracteres').len(3, 15);
-    // verificar se nome ja esta sendo utilizado
+    req.assert('apelido', 'Nome ou apelido eh obrigatorio').notEmpty();
+    req.assert('apelido', 'Nome ou apelido deve conter entre 3 e 15 caracteres').len(3, 15);
+    // verificar se nome ja esta sendo utilizado (se existe na list people)
     var erros = req.validationErrors();
 
-    if(erros) {
-        res.render('index', {validation: erros});
-        //encerra a execucao da funcao
+    if (erros) {
+        res.render('index', {
+            validation: erros
+        });
+        //render encerra a execucao da funcao
     }
 
-    res.render('chat');
+    //sending websocket message
+    application.get('io').emit(
+        'msgParaCliente', 
+        {apelido: dadosForm.apelido, mensagem: ' acabou de entrar no chat'}
+    );
+
+    //add user in the people list
+    if(people == null || people == '') {
+        people = [dadosForm.apelido];
+    } else {
+        people.push(dadosForm.apelido);
+    }
+
+    application.set('people', people);
+
+    //UPDATE PEOPLE LIST
+    application.get('io').sockets.emit(
+        'peopleParaCliente', 
+        {people: people}
+    );
+    
+
+    res.render('chat', {dadosForm: dadosForm, people: people});
 }
